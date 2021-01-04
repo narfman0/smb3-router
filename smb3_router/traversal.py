77e314960353238d60a892733a30563bc9823ef3
Jon Robison <narfman0@gmail.com>
2021-01-03 21:53:06 -0500

    -Flip how the recursion is done to make seeding more simple

--- smb3_router/traversal.py
+++ smb3_router/traversal.py
@@ -5,40 +5,38 @@ USE_ITEM_COST = 40  # this shouldnt be static, but using for simplicity
 
 def compute_path(graph, start_level_name="1-1", end_level_name="BC"):
     """ Create a path completing all required levels """
-    # TODO add constraints
-    # TODO add items like clouds
     # TODO add HBs and music boxes
+    # TODO observe enter/exit powerup state
     # TODO add edge costs like overworld movement, pipe transitions
+    # TODO add constraints e.g. no powerups
     cost_paths = []
     start_node = graph.find_start_node()
     end_node = graph.find_end_node()
-    items = [start_node.level.granted_item] if start_node.level.granted_item else []
-    create_path_permutations(
-        end_node, [start_node], start_node.level.frames, items, cost_paths
-    )
+    create_path_permutations(start_node, end_node, [], 0, [], cost_paths)
     return sorted(cost_paths, key=lambda cost_path: cost_path[0])[0]
 
 
-def create_path_permutations(end_node, path, cost, items, cost_paths):
-    current_node = path[-1]
-    if current_node == end_node:
+def create_path_permutations(node, end_node, path, cost, items, cost_paths):
+    path.append(node)
+    cost += node.level.frames
+    if node == end_node:
         # TODO make sure all required nodes are complete
-        cost_paths.append((cost, list(path)))
-        return
-    for next_node in current_node.next_nodes:
-        path.append(next_node)
-        if next_node.level.granted_item:
-            items.append(next_node.level.granted_item)
-        create_path_permutations(
-            end_node, path, cost + next_node.level.frames, items, cost_paths
-        )
-        del path[-1]
-        if not next_node.required:
-            for i, item in enumerate(items):
-                if item == Item.CLOUD:
-                    cloud_items = items.copy()
-                    del cloud_items[i]
-                    # TODO use clouds
-                    # create_path_permutations(
-                    #   end_node, path, cost + USE_ITEM_COST, cloud_items, cost_paths
-                    # )
+        cost_paths.append((cost, path.copy()))
+    if node.level.granted_item:
+        items.append(node.level.granted_item)
+    for next_node in node.next_nodes:
+        create_path_permutations(next_node, end_node, path, cost, items, cost_paths)
+    del path[-1]
+    if node.level.granted_item:
+        del items[-1]
+    cost -= node.level.frames
+    if not node.required:
+        for i, item in enumerate(items):
+            if item == Item.CLOUD:
+                pass
+                # TODO use clouds
+                # cloud_items = items.copy()
+                # del cloud_items[i]
+                # create_path_permutations(
+                #   end_node, path, cost + USE_ITEM_COST, cloud_items, cost_paths
+                # )
